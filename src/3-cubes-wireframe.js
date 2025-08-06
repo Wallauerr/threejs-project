6e51b1573bfe0ec1fc9553ba2f915accad4e2677
wallauerr <wallauer@protonmail.com>
2025-08-05 23:05:25 -0300

    separete leason of the group cubes

---
+++ src/3-cubes-wireframe.js
@@ -0,0 +1,66 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+
+// create scene
+const scene = new THREE.Scene()
+
+// add object to the scene
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 'gray', wireframe: true })
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
+cubeMesh.position.y = -1
+
+const cubeMesh2 = new THREE.Mesh(cubeGeometry, cubeMaterial)
+cubeMesh2.position.x = 2
+
+const cubeMesh3 = new THREE.Mesh(cubeGeometry, cubeMaterial)
+cubeMesh3.position.x = -2
+
+const group = new THREE.Group()
+group.add(cubeMesh)
+group.add(cubeMesh2)
+group.add(cubeMesh3)
+
+group.position.y = 2
+
+scene.add(group)
+
+//scene.add(cubeMesh)
+
+//cubeMesh.scale.set(2, 2, 1)
+
+const axesHelper = new THREE.AxesHelper(2)
+cubeMesh.add(axesHelper)
+
+// create the camera
+const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 35)
+camera.position.z = 5
+
+// create the renderer
+const canvas = document.querySelector('canvas.threejs')
+const renderer = new THREE.WebGLRenderer({
+	canvas: canvas,
+	antialias: true,
+})
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// instantiate the controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.autoRotate = false
+
+window.addEventListener('resize', () => {
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
+	renderer.setSize(window.innerWidth, window.innerHeight)
+})
+
+// render the scene
+const renderLoop = () => {
+	controls.update()
+	renderer.render(scene, camera)
+	window.requestAnimationFrame(renderLoop)
+}
+
+renderLoop()
